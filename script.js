6e5d0f563a79ff91274492995f8cb8beffb77bae
dexsha <41449574+dexsha@users.noreply.github.com>
2018-07-21 22:05:00 +0200

    Added features, sexy looks

--- script.js
+++ script.js
@@ -53,56 +53,87 @@ var handlers = {
 		addTodoText.value = "";
 		view.displayTodos();
 	},
-	editTodo: function(){
-		var editPosition = document.getElementById("editPosition");
-		var editTodoText = document.getElementById("editTodoText");
-		todoList.edit(editPosition.valueAsNumber, editTodoText.value);
-		editPosition.value = "";
-		editTodoText.value = "";
+	editTodo: function(position){
+		var editTextInput = document.getElementsByClassName('editTextInput')[0];
+		todoList.edit(position, editTextInput.value);
 		view.displayTodos();
 	},
 	removeTodo: function(position){
 		todoList.remove(position);
 		view.displayTodos();
 	},
-	toggleTodo: function(){
-		var togglePosition = document.getElementById("togglePosition");
-		todoList.toggleCompleted(togglePosition.valueAsNumber);
-		togglePosition.value = "";
+	toggleTodo: function(position){
+		todoList.toggleCompleted(position);
 		view.displayTodos();
-	},
+	}
 };
 
 var view = {
 	displayTodos: function(){
 		var todosUl = document.querySelector("ul");
+		var checkbox = document.getElementsByClassName("checkboxInput");
 		todosUl.innerHTML = "";
 
-		// todoList.todos.forEach(function(todo, position){
-			todoList.todos.forEach(function(todo, position){
+		todoList.todos.forEach(function(todo, position){
 			var todoLi = document.createElement("li");
-			var todoTextWithCompletion = '';
+			var liClass = 'liClass';
+			var isItChecked = false;
 
 			if(todo.completed === true){
-				todoTextWithCompletion = '(x) ' + todo.todoText;
+				liClass = 'liClassCompleted';
+				isItChecked = true;
 			}
-			else{
-				todoTextWithCompletion = '( ) ' + todo.todoText;
-			}
-
+			
 			todoLi.id = position;
-			todoLi.textContent = todoTextWithCompletion;
-			todoLi.className = 'liClass';
+			todoLi.textContent =  todo.todoText;
+			todoLi.className = liClass;
+			todoLi.appendChild(this.createCheckbox(isItChecked));
+			todoLi.appendChild(this.checkboxFA1());
+			todoLi.appendChild(this.checkboxFA2());
 			todoLi.appendChild(this.createDeleteButton());
+			todoLi.appendChild(this.createEditButton());
 			todosUl.appendChild(todoLi);
 		}, this);
 		getTodosFromLocal();
 	},
+	createCheckbox: function(isItChecked){
+		var checkbox = document.createElement('input');
+		checkbox.type = 'checkbox';
+		checkbox.className = 'checkboxInput';
+		checkbox.checked = isItChecked;		
+		return checkbox;
+	},
+	checkboxFA1: function(){
+		var checkboxFA1 = document.createElement('i');
+		checkboxFA1.className = 'far fa-circle';
+		return checkboxFA1;
+	},
+	checkboxFA2: function(){
+		var checkboxFA2 = document.createElement('i');
+		checkboxFA2.className = 'far fa-check-circle';
+		return checkboxFA2;
+	},
 	createDeleteButton: function(){
-		var deleteButton = document.createElement('button');
-		deleteButton.textContent = 'Delete';
-		deleteButton.className = 'deleteButton';
+		var deleteButton = document.createElement('i');
+		deleteButton.className = 'fas fa-times';
 		return deleteButton;
+	},
+	createEditButton: function(){
+		var editButton = document.createElement('i');
+		editButton.className = 'far fa-edit';
+		return editButton;
+	},
+	createEditTextInput: function(position){
+		var editTextInput = document.createElement('input');
+		editTextInput.type = 'text';
+		editTextInput.className = 'editTextInput';
+		editTextInput.value = todoList.todos[position].todoText;
+		return editTextInput;
+	},
+	createConfirmEditButton: function(position){
+		var confirmEditButton = document.createElement('i');
+		confirmEditButton.className = 'fas fa-check';
+		return confirmEditButton;
 	},
 	setUpEventListeners: function(){
 		var todosUl = document.querySelector('ul');
@@ -112,8 +143,23 @@ var view = {
 			var elementClicked = event.target;
 
 			// Check if elementClicked is a delete button
-			if(elementClicked.className === 'deleteButton'){
+			if(elementClicked.className === 'fas fa-times'){
 				handlers.removeTodo(parseInt(elementClicked.parentNode.id));
+			}
+			if(elementClicked.className === 'checkboxInput'){
+				handlers.toggleTodo(parseInt(elementClicked.parentNode.id));
+			}
+			if(elementClicked.className === 'far fa-edit'){
+				view.displayTodos();
+				var todoLi = document.getElementById(elementClicked.parentNode.id)
+				todoLi.textContent = "";
+				var editButton = document.getElementsByClassName('editButton');
+				todoLi.appendChild(view.createEditTextInput(parseInt(elementClicked.parentNode.id)));
+				todoLi.appendChild(view.createConfirmEditButton(parseInt(elementClicked.parentNode.id)));
+				//todoLi.removeChild(editButton[parseInt(elementClicked.parentNode.id)]);
+			}
+			if(elementClicked.className === 'fas fa-check'){
+				handlers.editTodo(parseInt(elementClicked.parentNode.id));
 			}
 		});	
 	}
@@ -135,7 +181,7 @@ function getTodosFromLocal(){
 	var str = localStorage.getItem("todos");
 	todoList.todos = JSON.parse(str);
 	if(!todoList.todos){
-		todos = [];
+		todoList.todos = [];
 	}
 }
 
